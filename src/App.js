133358ff98393c27f32f6cab7c55108a6d26ea14
Diana Nemcekova <dianka.melegova@gmail.com>
2022-07-21 17:01:04 +0200

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
 const App = () => {
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container"></div>
+        <h2>Hats</h2>
+        <p>Shop Now</p>
       </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container"></div>
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
       </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container"></div>
+        <h2>Sneakers</h2>
+        <p>Shop Now</p>
       </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container"></div>
+        <h2>Womens</h2>
+        <p>Shop Now</p>
       </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
+      <div className="category-container">
+        {/* <img src="" alt="" /> */}
+        <div className="category-body-container"></div>
+        <h2>Mens</h2>
+        <p>Shop Now</p>
       </div>
     </div>
   );
