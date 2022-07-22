1b952034ef1ec5fada0decd1490c8f5894a94a88
Diana Nemcekova <dianka.melegova@gmail.com>
2022-07-22 13:10:23 +0200

    firebase added, Route to shop and sign in added, navigation and styles added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import HomePage from "./routes/home/home-page.component";
 import { Routes, Route } from "react-router-dom";
+import HomeNavigation from "./routes/navigation/home-navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return <div>I am the shop page</div>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/" element={<HomeNavigation />}>
+        <Route index element={<HomePage />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+        <Route path="sign-in" element={<SignIn />}></Route>
+      </Route>
     </Routes>
   );
 };
