0e8d3d8243e4716ddc25dc9243423c0dff632f7b
Diana Nemcekova <dianka.melegova@gmail.com>
2022-07-28 15:43:34 +0200

    minor styling changes, links to categories added

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -16,9 +16,11 @@ const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <span>Your cart is empty</span>
+        )}
       </div>
       <CustomButton onClick={goToCheckoutHandler}>GO TO CHECKOUT</CustomButton>
     </div>
--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,17 +1,18 @@
 import "./category-item.styles.scss";
+import { Link } from "react-router-dom";
 
 const CategoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
   return (
     <div className="category-item-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
-      <div className="category-body-container">
+      <Link to={route} className="category-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
+      </Link>
     </div>
   );
 };
